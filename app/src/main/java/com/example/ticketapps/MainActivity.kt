f7ea4863e63469465394f1fcf7fd3953c23fc7e0
Budi Darmawan <budymitman_77@yahoo.co.id>
2020-10-16 14:49:42 +0800

    commit2

--- app/src/main/java/com/example/ticketapps/MainActivity.kt
+++ app/src/main/java/com/example/ticketapps/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ticketapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(DetailBookingFragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 }
