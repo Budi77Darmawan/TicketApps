0277b5d276ee868de57283c89ff35da22c1c0191
wahyukurniawaan <wahyukurniawaan@gmail.com>
2020-10-19 20:44:01 +0700

    menambahkan Search Result Activity

--- app/src/main/java/com/example/ticketapps/SearchFlightActivity.kt
+++ app/src/main/java/com/example/ticketapps/SearchFlightActivity.kt
@@ -1,11 +1,23 @@
 package com.example.ticketapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.ticketapps.databinding.ActivitySearchFlightBinding
+import com.example.ticketapps.searchResult.SearchResultActivity
 
 class SearchFlightActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchFlightBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_flight)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search_flight)
+        binding.buttonSearchFlight.setOnClickListener {
+            val intent = Intent(this, SearchResultActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
